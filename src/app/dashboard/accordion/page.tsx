01d4e0f28206a02ff6f7a647e5ef43b120592ade
Luis Matus <luis.matus.jr@gmail.com>
2024-09-09 16:56:47 -0600

    first commit

--- src/app/dashboard/accordion/page.tsx
+++ src/app/dashboard/accordion/page.tsx
@@ -1,7 +1,38 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
 export default function Page() {
+  const items = [
+    {
+      id: "item-1",
+      question: "Is it accesible?",
+      answer: "Yes. It adheres to the WAI-ARIA design pattern",
+    },
+    {
+      id: "item-2",
+      question: "How do I get started?",
+      answer: "You can start by reading the documentation.",
+    },
+    {
+      id: "item-3",
+      question: "Can I use it on mutiple projects?",
+      answer: "Yes. It's licensed under MIT license",
+    },
+  ];
   return (
     <div>
-      <h1>Accordion Page</h1>
+      <Accordion type="single" collapsible className="w-full">
+        {items.map((item) => (
+          <AccordionItem key={item.id} value={item.id}>
+            <AccordionTrigger>{item.question}</AccordionTrigger>
+            <AccordionContent>{item.answer}</AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
     </div>
   );
 }
--- src/app/dashboard/alert/page.tsx
+++ src/app/dashboard/alert/page.tsx
@@ -1,7 +1,30 @@
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { CheckIcon, RocketIcon } from "@radix-ui/react-icons";
+
 export default function Page() {
   return (
-    <div>
-      <h1>Alert Page</h1>
+    <div className="grid gap-3">
+      <Alert>
+        <RocketIcon className="h-4 w-4" />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components to your app using the cli.
+        </AlertDescription>
+      </Alert>
+      <Alert variant={"destructive"}>
+        <RocketIcon className="h-4 w-4" />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components to your app using the cli.
+        </AlertDescription>
+      </Alert>
+      <Alert variant={"success"}>
+        <CheckIcon className="h-4 w-4" />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components to your app using the cli.
+        </AlertDescription>
+      </Alert>
     </div>
   );
 }
