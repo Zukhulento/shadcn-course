b95a47d6753c2b5e8323e2e8eecca8d1b1fea447
lmatus3 <lmatus3@unica.edu.ni>
2024-09-23 17:03:27 -0600

    respaldo de avances de proyecto de componentes

--- src/app/dashboard/card/page.tsx
+++ src/app/dashboard/card/page.tsx
@@ -7,6 +7,8 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
 export default function Page() {
   return (
@@ -26,6 +28,26 @@ export default function Page() {
           </CardFooter>
         </Card>
       ))}
+      <Card className="col-span-2">
+      <CardHeader>
+        <CardTitle>Create project</CardTitle>
+        <CardDescription>Deploy your new project in one-click.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" placeholder="Name of your project" />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button variant="outline">Cancel</Button>
+        <Button>Deploy</Button>
+      </CardFooter>
+    </Card>
     </div>
   );
 }
---
+++ src/app/dashboard/carousel/page.tsx
@@ -0,0 +1,31 @@
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+export default function Page() {
+  return (
+    <div className="w-full flex justify-center">
+      <Carousel className="w-full max-w-xs" opts={{ dragFree: true }}>
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index}>
+              <div className="p-1">
+                <Card>
+                  <CardContent className="flex aspect-square items-center justify-center p-6">
+                    <span className="text-4xl font-semibold">{index + 1}</span>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="hidden sm:flex justify-center items-center" />
+        <CarouselNext className="hidden sm:flex justify-center items-center" />
+      </Carousel>
+    </div>
+  );
+}
